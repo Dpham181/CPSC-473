c33fe59c51628f9b060b33f58e581a8ff73d1362
danhpham <danhpham312@gmail.com>
2018-10-19 02:41:01 -0700

    Updated

--- HW 2/js/events.js
+++ HW 2/js/events.js
@@ -16,7 +16,9 @@ class student {
 let students = [
   new student("Danh", "Pham"),
   new student("Henrry", "Do"),
-  new student("Thomas", "Ngo")
+  new student("Thomas", "Ngo"),
+  new student("Danny", "Pham"),
+  new student("Steven", "Pham")
 ];
 // defind class for course
 class course{
@@ -48,32 +50,32 @@ let gpas = [
 
 let students_records = [
 {
-    name: students[0].lastname,
+    name: students[0].firstname,
     course: courses[0].Course,
     gpa: gpas[1].gpa
 
 },
 {
-    name: students[1].lastname,
+    name: students[1].firstname,
     course: courses[2].Course,
     gpa: gpas[2].gpa
 
 },
 {
-    name: students[2].lastname,
+    name: students[2].firstname,
     course: courses[2].Course,
     gpa: gpas[2].gpa
 
 },
 
 {
-    name: students[1].lastname,
+    name: students[1].firstname,
     course: courses[1].Course,
     gpa: gpas[1].gpa
 
 },
 {
-    name: students[0].lastname,
+    name: students[0].firstname,
     course: courses[1].Course,
     gpa: gpas[2].gpa
 
@@ -104,9 +106,50 @@ function addFormListener() {
     }
       console.log(lastname[0]);
       // getting id for table before use to generate records
-      var items_table = document.getElementById('items_table');
+      var students_table = document.getElementById('student_table');
+      var student_firstname = null;
+      for ( var i = 0 ; i < students.length ; i++){
+
+        // searching to our records with the input name from users
+        if  (students[i].lastname == lastname[0]){
+
+            console.log( students[i].fullname );
+            // generate table with inner html method
+           students_table.innerHTML+= '<tr><td><button id="button" value= "'  +students[i].firstname+ '">'+ students[i].fullname + ' </button></td><td>';
+
+                }
+
+
+      }
+      students_table.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var elm = document.getElementById('button');
+        var firstname = elm.value;
+        console.log(firstname);
+        var records_table = document.getElementById('records_table');
+
+          for ( var i = 0 ; i < students_records.length ; i++){
+
+            // searching to our records with the input name from users
+            if  (students_records[i].name == firstname){
+
+                console.log( students_records[i].course );
+                console.log( students_records[i].gpa );
+
+                // generate table with inner html method
+    	           records_table.innerHTML+= '<tr><td>' + students_records[i].course + '</td><td>' + students_records[i].gpa + '</td></tr>';
+
+                    }
+
+
+          }
+});
 
 // looping to records to see with there any name that matches
+/*
+    var items_table = document.getElementById('records_table');
+
       for ( var i = 0 ; i < students_records.length ; i++){
 
         // searching to our records with the input name from users
@@ -119,9 +162,10 @@ function addFormListener() {
 	           items_table.innerHTML+= '<tr><td>' + students_records[i].course + '</td><td>' + students_records[i].gpa + '</td></tr>';
 
                 }
-            
+
 
       }
+*/
 
 		});
 	}
